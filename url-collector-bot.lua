7158f383887e761e40a73dd052ddfc050f870bf9
cldrn <paulino@calderonpale.com>
2016-11-06 13:25:55 -0600

    Adds URL bot collector

---
+++ url-collector-bot.lua
@@ -0,0 +1,57 @@
+#!/usr/bin/lua
+--[[
+url-collector-bot.lua - Telegram bot to save URLs shared in a channel/conversation.
+
+The bot saves the URLs in a text file with the format '<url> [<author]'.
+
+Author: @calderpwn
+Dependencies: lua-telegram-bot [https://github.com/cosmonawt/lua-telegram-bot]
+]]
+
+--Telegram API Token
+local token = ""
+local bot, extension = require("lua-bot-api").configure(token)
+
+--onMessageReceive handler.
+extension.onTextReceive = function (msg)
+  print(string.format("[%s] Incoming message from '%s':\n%s", os.date(), msg.from.first_name, msg.text))
+  msg.text = string.lower(msg.text)
+  if msg.entities then
+  for _, x in pairs(msg.entities) do
+    if x.type == "url" then
+      local url = string.sub(msg.text, x.offset, x.offset + x.length)
+      write_url(url, msg.from.first_name)
+      bot.sendMessage(msg.chat.id, string.format("URL '%s' saved.", url))
+    end
+  end
+  end
+  if (msg.text == "/hi") then
+    bot.sendMessage(msg.chat.id, "Hello, I'm " .. bot.first_name)
+  elseif (msg.text == "/ping") then
+    bot.sendMessage(msg.chat.id, "pong")
+  elseif (msg.text == "/list") then
+    bot.sendMessage(msg.chat.id, get_urls())
+  elseif (msg.text == "/help") or (msg.text == "/man") then
+    bot.sendMessage(msg.chat.id, "/ping: Checks if I'm alive.\n/list: Returns a list of saved URLs.")
+  end
+end
+
+--Retrieves stored URLs.
+function get_urls()
+   local urls = "URL list:\n"
+   local lines = {}
+   for line in io.lines("urls.txt") do
+      urls = urls .. line .. "\n"
+   end
+   return urls
+end
+
+--Stores URL in file. Each URL is stored as a new line.
+function write_url(url, author)
+  local file = io.open("urls.txt", "a")
+  io.output(file)
+  io.write(string.format("%s [%s]\n", url, author))
+  io.close(file)
+end
+
+extension.run()
